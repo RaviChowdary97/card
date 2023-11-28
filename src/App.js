09cad4e7fb46e0ca7e01010be17da15099b9a6d6
RaviChowdary97 <aklvvsr1999@gmail.com>
2023-11-28 21:48:04 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card1 />
+      <Card2 />
+      <Card3 />
+    </div>
+  );
+}
+
+function Card1() {
+  return (
+    <div className="Card">
+      <div className="third">
+        <p>FREE</p>
+        <h1>$0/month</h1>
+      </div>
+      <hr />
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Single User
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> 50GB Storage
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Unlimited Public Project
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Community Access
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-xmark fa-sm"></i> Unlimited Private Projects
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-xmark fa-sm"></i> Dedicated Phone Support
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-xmark fa-sm"></i> Free Subdomain
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-xmark fa-sm"></i> Monthly Status Report
+        </span>
+      </div>
+      <button>BUTTON</button>
+    </div>
+  );
+}
+function Card2() {
+  return (
+    <div className="Card1">
+      <div className="second">
+        <p>PLUS</p>
+        <h1>$9/month</h1>
+      </div>
+      <hr />
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Single User
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> 50GB Storage
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Unlimited Public Project
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Community Access
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Unlimited Private Projects
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Dedicated Phone Support
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Free Subdomain
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-xmark fa-sm"></i> Monthly Status Report
+        </span>
+      </div>
+      <button>BUTTON</button>
+    </div>
+  );
+}
+
+function Card3() {
+  return (
+    <div className="Card2">
+      <div className="first">
+        <p>PRO</p>
+        <h1>$49/month</h1>
+      </div>
+      <hr />
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Single User
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> 50GB Storage
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Unlimited Public Project
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Community Access
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Unlimited Private Projects
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Dedicated Phone Support
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Free Subdomain
+        </span>
+      </div>
+      <div className="First">
+        <span>
+          <i class="fa-solid fa-check fa-sm"></i> Monthly Status Report
+        </span>
+      </div>
+      <button className="button">BUTTON</button>
     </div>
   );
 }
